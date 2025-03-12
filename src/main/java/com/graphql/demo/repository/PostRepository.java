785eb0ad515b625f06c1bb2db0e6311fd8397fde
Oussema Sahbeni <oussemasahbeni300@gmail.com>
2025-03-12 22:21:52 +0100

    Add search functionality for posts and update PostRepository for GraphQL integration

--- src/main/java/com/graphql/demo/repository/PostRepository.java
+++ src/main/java/com/graphql/demo/repository/PostRepository.java
@@ -3,11 +3,14 @@ package com.graphql.demo.repository;
 import com.graphql.demo.entity.Post;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.graphql.data.GraphQlRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface PostRepository extends JpaRepository<Post, UUID> {
+@GraphQlRepository
+public interface PostRepository extends JpaRepository<Post, UUID>, QueryByExampleExecutor<Post> {
     Page<Post> findByTitleContainingIgnoreCase(String searchCriteria, Pageable pageable);
 
     List<Post> findByAuthorId(UUID id);
